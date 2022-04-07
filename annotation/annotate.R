99895708e48dc15ce710b061dd2ddf56f760826c
aleks <alekso.bekker@gmail.com>
2022-04-07 14:56:59 -0400

    Create basic framework for adding tools

--- annotation/annotate.R
+++ annotation/annotate.R
@@ -1,34 +1,57 @@
 
 #--- Load general packages ---#
 
-#--- Functions for annotation tools ---#
+library(magrittr)
+
+#--- Util functions ---#
+
+convert_confidence_to_ranks(confidence_mat, decreasing = TRUE) {
+  sapply(1:ncol(confidence_mat),
+         function(i) confidence_mat[,i] %>% order(decreasing = decreasing) %>% order) %>%
+    return
+}
+
+#--- SingleR Functions ---#
 
 annotate_SingleR <- function(expr_data, ref_data, ...) {
-  return(SingleR::SingleR(test = expr_data,
-                          ref = ref_data,
-                          assay.type.test = 1))
+  SingleR::SingleR(test = expr_data,
+                   ref = ref_data,
+                   assay.type.test = 1) %>% return
+}
+
+convert_SingleR_to_ranks <- function(res, use_reciprocal = TRUE) {
+  convert_confidence_to_ranks(res$scores) %>% return
 }
 
 #--- Main annotation functions ---#
 
 select_annot_funcs <- function(annot_type, tools.to_use,
   tools.avilable = list(
-    ref = list('SingleR' = annotate_SingleR, 'test' = print),
+    ref = list('SingleR' = annotate_SingleR),
     marker = list()
   )) {
   
-  if (identical(tools.to_use, '*')) {
-    return(tools.avilable[[annot_type]])
-  } else {
-    return(tools.avilable[[annot_type]][tools.to_use])
+  to_return = tools.avilable[[annot_type]]
+  
+  if (!identical(tools.to_use, '*')) {
+    to_return <- to_return[tools.to_use]
   }
+  
+  return(to_return)
 }
 
+# Description:   
+# Params:
+#   -expr_data (SeuratObject): Experimental data for cells to be labelled
+#   -ref_data (SeuratObject): Reference/marker data to be used in the tools
+#   -annot_type (character): Type of CTA to perform <"ref"/"marker">
+#   -annot_tools (character): Which tools of type `annot_type` to use. '*' selects all tools
+# Outputs: TBD
 annotate <- function(expr_data, ref_data, annot_type, annot_tools = '*', ...) {
   tools_funcs <- select_annot_funcs(annot_type, annot_tools)
   
   for (tools_func in tools_funcs) {
-    tools_func(expr_data, ref_data, ...)
+    tools_func(expr_data, ref_data, ...) # manage returned data
   }
 }
 
