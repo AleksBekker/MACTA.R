645e62eba5505e46da4b327debdb0a2fc3d84aef
aleks <alekso.bekker@gmail.com>
2022-04-16 20:21:16 -0400

    Create CTA Tool class

---
+++ annotation/tools/cta_tool.R
@@ -0,0 +1,68 @@
+
+library(methods)
+
+library(magrittr)
+
+# `CTATool` class declaration --------------------------------------------------
+
+CTATool <- setClass(
+
+    # Name
+    "CTATool",
+
+    # Slots
+    slots = c(
+        annotate_func,
+        convert_func,
+    ),
+
+    # Default values
+    prototype = list(
+        annotate_func = function(expr_data, ref_data, labels_col, ...) {
+            stderr("`annotation_func` must be implemented by subclasses")
+        },
+        convert_func = function(results, convert_to, ...) {
+            stderr("`conversion_func` must be implemented by subclasses")
+        }
+    )
+)
+
+# `annotate_func` getters and setters ------------------------------------------
+
+setGeneric("annotate_func", function(x) standardGeneric("annotate_func"))
+setGeneric("annotate_func<-", function(x, v) standardGeneric("annotate_func<-"))
+
+# TODO: add documentation
+setMethod("annotate_func", "CTATool", function(x) x@annotate_func)
+setMethod("annotate_func<-", "CTATool", function(x, v) {
+    x@annotate_func <- value
+})
+
+# `convert_func` getters and setters -------------------------------------------
+
+setGeneric("convert_func", function(x) standardGeneric("convert_func"))
+setGeneric("convert_func<-", function(x, v) standardGeneric("convert_func<-"))
+
+# TODO: add documentation
+setMethod("convert_func", "CTATool", function(x) x@convert_func)
+setMethod("convert_func<-", "CTATool", function(x, v) {
+    x@convert_func <- v
+})
+
+# `run_full` function implementation -------------------------------------------
+
+setGeneric(
+    "run_full",
+    function(cta_tool, expr_data, ref_data, labels_col, convert_to, ...) {
+        standardGeneric("run_full")
+    }
+)
+
+setMethod(
+    "run_full",
+    definition = function(cta_tool, expr_data, ref_data, labels_col, convert_to, ...) {
+        annotate_func(cta_tool)(expr_data, ref_data, labels_col, ...) %>%
+            convert_func(cta_tool)(convert_to, ...) %>%
+            return()
+    }
+)
